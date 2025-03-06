e0bc71176933ab40097f9deb48acedf93d630258
Kadayo-Hamm <daolegit@gmail.com>
2025-03-06 10:25:47 -0700

    Create march_6.cs

---
+++ march_6.cs
@@ -0,0 +1,9 @@
+//Happy Ghana Birthday
+//Today I learned about floating point literals
+Console.WriteLine('b');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
